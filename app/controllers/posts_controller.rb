2a2c08f6c774f8e44c1e07b66c9efb9651a2ce8c
“RenHamamoto” <rentaro0831@gmail.com>
2020-03-29 01:09:05 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to posts_url, notice: "hikidashi「#{post.name}」を更新しました。"
   end
 
   private
