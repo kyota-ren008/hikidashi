9568e2cf88bc4509695d45ddbf47eea053cebbd1
“RenHamamoto” <rentaro0831@gmail.com>
2020-03-28 15:37:07 +0900

    新規登録機能・続

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  #登録フォームから送られてきたデータをデータベースに保存して、一覧画面に遷移する
+  #redirect_toには特定の「Flashメッセージ」を渡すことができる。表示するためにはlayoutsに記述する
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "hikidashi「#{post.name}」を登録しました。"
+  end
+
   def edit
+  end
+
+  private
+  
+  #受け取る情報だけを抜き取って、登録する役割
+  def post_params
+    params.require(:post).permit(:name, :description, :image)
   end
 end
